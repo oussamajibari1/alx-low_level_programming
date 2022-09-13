acac8b58cb893397fee61bd996860104d4520272
oussamajibari1 <111062069+oussamajibari1@users.noreply.github.com>
2022-09-13 02:19:46 +0000

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,32 +1,50 @@
-#include<stdio.h>
-#include<string.h>
-#define N 10
- 
-void print(int *num, int n)
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-    int i;
-    for ( i = 0 ; i < n ; i++)
-        printf("%d ", num[i]);
-    printf("\n");
-}
-int main()
-{
-    int num[N];
-    int *ptr;
-    int temp;
-    int i, n, j;
-    printf("\nHow many number you want to enter: ");
-        scanf("%d", &n);
-    printf("\nEnter a list of numbers to see all combinations:\n");
-    for (i = 0 ; i < n; i++)
-        scanf("%d", &num[i]);
-    for (j = 1; j <= n; j++) {
-        for (i = 0; i < n-1; i++) {
-            temp = num[i];
-            num[i] = num[i+1];
-            num[i+1] = temp;
-            print(num, n);
+	int p = 48, q = 48, y = 48, x = 49;
+
+	while (p < 58)
+	{
+		while (q < 58)
+		{
+			while (y < 58)
+			{
+				while (x < 58)
+				{
+					putchar(p);
+					putchar(q);
+					putchar(' ');
+					putchar(y);
+					putchar(x);
+					if (!(p == 57 &&
+						q == 56 &&
+						y == 57 &&
+						x == 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					x++;
+				}
+				y++;
+				x = 48;
+			}
+			q++;
+			q = p;
+			x = q + 1;
+		}
+		p++;
+		q = 48;
+		y = p;
+		x = q+ 1;
 	}
-    }
-    return 0;
+	putchar('\n');
+	return (0);
 }
