c1bc7d7a45b06875b4a42958792df813a4f51aac
oussamajibari1 <111062069+oussamajibari1@users.noreply.github.com>
2022-09-27 18:08:23 +0100

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,34 +1,28 @@
 #include "holberton.h"
 
 /**
- * _strpbrk - Search a string for any of a set of bytes.
- * @s: string
- * @accept: string to match
- * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
- * or NULL if no such byte is found.
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: The string to be searched.
+ * @accept: The set of bytes to be searched for.
+ *
+ * Return: If a set is matched - a pointer to the matched byte.
+ *         If no set is matched - NULL.
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
-	char *p;
+	int i;
 
-	i = 0;
-	while (s[i] != '\0')
+	while (*s)
 	{
-		j = 0;
-		while (accept[j] != '\0')
+		for (i = 0; accept[i]; i++)
 		{
-			if (accept[j] == s[i])
-			{
-				p = &s[i];
-				return (p);
-			}
-			j++;
+			if (*s == accept[i])
+				return (s);
 		}
-		i++;
+
+		s++;
 	}
 
-	return (0);
+	return ('\0');
 }
-Footer
